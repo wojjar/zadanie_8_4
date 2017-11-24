45ca91cf84ed25784fcf8660f3aec01135e53ca6
Jarosław Wójcik <j.wojcik@interia.eu>
2017-11-24 12:55:30 +0100

    Add js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+  //alert("Hello World");
+for (var i=1; i<=2; i++) {               //Pętla for ze zmienną i .
+    document.write( "Hello ", i,"<br>");
+    alert("Hello " +i);
+}
+document.write("<br>");
+/*for (var a=1; a<=2; a++) { 
+    document.write( "Hello ", a,"<br>");
+}*/
+for (var j=1; j<=2; j++) {                 //Pętla for ze zmienną j .
+    document.write( "Hello ", j,"<br>");
+    alert("Hello " +j);
+}
